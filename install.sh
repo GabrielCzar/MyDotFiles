6bf5b6b0445eccc24f03485be8bcccba8dac3918
Gabriel Cesar <gabrielcesar.a.l@gmail.com>
2020-05-30 20:45:52 -0300

    fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if command -v git >/dev/null 2>&1; then
+  git clone https://github.com/gabrielczar/mydotfiles.git
+  cd mydotfiles
+elif command -v unzip >/dev/null 2>&1; then
+  wget -O mydotfiles.zip https://github.com/gabrielczar/mydotfiles/archive/master.zip
+  unzip mydotfiles.zip
+  cd mydotfiles
+else
+  echo "Unable to download repository."
+  exit 1
+fi
+
 sudo bash workstation.sh
 
 bash git_config_global.sh
