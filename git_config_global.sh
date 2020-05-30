a1c9d1424baeb78680273a05189ebec7630755af
Gabriel César <gabrielcesar.a.l@gmail.com>
2020-05-30 18:50:19 -0300

    Update git_config_global.sh

--- git_config_global.sh
+++ git_config_global.sh
@@ -6,7 +6,7 @@ function _configure_git_template_dir {
 }
 
 function _install_jira_prepare_commit_msg {
-  wget https://gist.githubusercontent.com/GabrielCzar/e1dc9773830701015699fbc574b7e46c/raw/589167cbf1b6e005993ebfffbc7357e8b6ca182b/prepare-commit-msg -P ~/.git-templates/hooks
+  wget https://gist.githubusercontent.com/GabrielCzar/e1dc9773830701015699fbc574b7e46c/raw/c870020f05e4e4b61225be88ea1357416fd4d451/prepare-commit-msg -P ~/.git-templates/hooks
   chmod +x ~/.git-templates/hooks/prepare-commit-msg
 }
 
