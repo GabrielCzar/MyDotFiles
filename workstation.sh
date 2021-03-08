f520ce6c774cb726328003a80b17e7eaacb42d69
Gabriel César <gabrielcesar.a.l@gmail.com>
2021-03-07 21:33:23 -0300

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -11,16 +11,9 @@ sudo apt update &&
 
   ## python-pip nautilus-dropbox not found in ubuntu-20
   ## Installing packages and softwares from Ubuntu deb repository ##
-  sudo apt install git maven python3 docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak yarn -y &&
+  sudo apt install git maven python3 git build-essential libssl-dev flatpak gnome-software-plugin-flatpak yarn -y &&
   sudo apt install ruby-full -y &&
   sudo apt-get install --no-install-recommends gnome-panel -y &&
-
-  ## Installing Snap packages ##
-  sudo snap install code --classic &&
-  sudo snap install --edge node --classic &&
-  sudo snap install insomnia &&
-  sudo snap install spotify &&
-  sudo snap install intellij-idea-ultimate --classic &&
 
   ## Adicionando repositório Flathub ##
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
@@ -29,6 +22,10 @@ sudo apt update &&
   sudo flatpak install flathub com.sublimetext.three -y &&
 
   # Installing tools
+  sudo apt install docker docker-compose &&
+  sudo groupadd docker &&
+  sudo usermod -aG docker $USER &&
+  newgrp docker &&
   sudo apt install gnome-tweak-tool -y &&
   sudo apt install guake -y &&
   sudo apt install curl -y &&
@@ -37,15 +34,19 @@ sudo apt update &&
   sudo apt install zsh -y &&
   chsh -s $(which zsh) -y &&
   sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y &&
-  sudo groupadd docker &&
-  sudo usermod -aG docker $USER &&
-  newgrp docker &&
   wget https://nosqlbooster.com/s3/download/releasesv5/nosqlbooster4mongo-5.2.12.AppImage &&
   chmod a+x nosqlbooster4mongo-5.2.12.AppImage &&
   sudo apt install htop -y &&
   sudo apt-get install openvpn -y &&
   sudo apt-get install network-manager-openvpn-gnome -y &&
   sudo apt install deepin-screenshot -y &&
+
+  ## Installing Snap packages ##
+  sudo snap install code --classic &&
+  sudo snap install --edge node --classic &&
+  sudo snap install insomnia &&
+  sudo snap install spotify &&
+  sudo snap install intellij-idea-ultimate --classic &&
 
   ## Atualização do sistema ##
   sudo apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
